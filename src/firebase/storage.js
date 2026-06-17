9a7e637559bf6ef95b7d3e1e018c43666dc09d70
rodrigocabrito <rodri.cabrito@gmail.com>
2026-06-17 23:14:50 +0100

    Deleted older unused files

--- src/firebase/storage.js
+++
@@ -1,8 +0,0 @@
-import { storage } from './config';
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-
-export const uploadProfilePicture = async (uid, file) => {
-  const storageRef = ref(storage, `profile-pictures/${uid}`);
-  const snapshot = await uploadBytes(storageRef, file);
-  return await getDownloadURL(snapshot.ref);
-};
